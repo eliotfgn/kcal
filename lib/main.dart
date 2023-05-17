132ea0865c6e3abdccbf8ac404a1b5f2dc273528
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-17 14:08:13 +0100

    feat: add and config splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kcal/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const App());
@@ -14,7 +15,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
+    return MaterialApp(
+      routes: {
+        "/": (context) => const SplashScreen(),
+      },
+      initialRoute: "/",
     );
   }
 }
