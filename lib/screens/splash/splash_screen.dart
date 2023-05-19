460c371c30df275e9cbf64e89e5dc371e926e8b4
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-19 17:13:12 +0100

    feat: splash screen layout

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:kcal/constants/color_constant.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: ColorConstant.primaryGreen,
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          const Spacer(),
+          Image.asset("assets/images/kcal-logo.png"),
+          const Spacer(),
+          Text("ZUAMICA"),
+          const SizedBox(height: 95),
+        ],
+      )
+    );
   }
 }
