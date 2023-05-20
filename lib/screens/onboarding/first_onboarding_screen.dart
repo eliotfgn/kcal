6b75bc2657bcde1d4eb1672436996acb93ada8ed
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-20 20:18:49 +0100

    refactor: add onboarding screen template class

--- lib/screens/onboarding/first_onboarding_screen.dart
+++ lib/screens/onboarding/first_onboarding_screen.dart
@@ -1,66 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:kcal/constants/color_constant.dart';
-import 'package:kcal/constants/text_constant.dart';
-import 'package:kcal/widgets/custom_button.dart';
-import 'package:kcal/widgets/onboarding_stepper.dart';
+import 'package:kcal/screens/onboarding/onboarding_screen.dart';
 
 class FirstOnboardingScreen extends StatelessWidget {
   const FirstOnboardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Spacer(),
-            Center(
-              child: Image.asset("assets/images/kcal-logo-green.png"),
-            ),
-            const Spacer(),
-            Image.asset("assets/images/onboarding1-hero.png"),
-            const Spacer(),
-            Text(
-              "Eat healthy",
-              style: TextConstant.onboardingTitle,
-            ),
-            SizedBox(
-              width: 275,
-              child: Text(
-                "Maintaining good health should be the primary focus of everyone.",
-                style: TextConstant.onboardingText,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(height: 12),
-            const OnboardingStepper(
-              currentStep: 1,
-            ),
-            const Spacer(),
-            const CustomButton(
-              text: "Getting started",
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already Have An Account?",
-                  style: TextConstant.onboardingText,
-                ),
-                const SizedBox(width: 5),
-                Text(
-                  'Log in',
-                  style: TextConstant.onboardingText
-                      .copyWith(color: ColorConstant.primaryGreen),
-                ),
-              ],
-            ),
-            const Spacer(),
-          ],
-        ),
-      ),
+    return const OnboardingScreen(
+      imagePath: "assets/images/onboarding1-hero.png",
+      title: "Eat healthy",
+      text:
+          "Maintaining good health should be the primary focus of everyone.hashCode",
+      currentStep: 1,
+      nextStep: "/second-onboarding",
     );
   }
 }
---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:kcal/constants/color_constant.dart';
+import 'package:kcal/constants/text_constant.dart';
+import 'package:kcal/widgets/custom_button.dart';
+import 'package:kcal/widgets/onboarding_stepper.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({
+    Key? key,
+    required this.imagePath,
+    required this.title,
+    required this.text,
+    required this.currentStep,
+    required this.nextStep,
+  }) : super(key: key);
+
+  final String imagePath;
+  final String title;
+  final String text;
+  final int currentStep;
+  final String nextStep;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Center(
+              child: Image.asset("assets/images/kcal-logo-green.png"),
+            ),
+            const Spacer(),
+            Image.asset(imagePath),
+            const Spacer(),
+            Text(
+              title,
+              style: TextConstant.onboardingTitle,
+            ),
+            SizedBox(
+              width: 275,
+              child: Text(
+                text,
+                style: TextConstant.onboardingText,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 12),
+            OnboardingStepper(
+              currentStep: currentStep,
+            ),
+            const Spacer(),
+            const CustomButton(
+              text: "Getting started",
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already Have An Account?",
+                  style: TextConstant.onboardingText,
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  'Log in',
+                  style: TextConstant.onboardingText
+                      .copyWith(color: ColorConstant.primaryGreen),
+                ),
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
