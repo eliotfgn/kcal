39180f37fc22c64ddc625d5aea762aa6410ddeb6
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-22 10:32:41 +0100

    feat: implement camera interface

--- lib/screens/scan/scan_screen.dart
+++ lib/screens/scan/scan_screen.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:kcal/constants/color_constant.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({Key? key}) : super(key: key);
@@ -20,7 +21,7 @@ class _ScanScreenState extends State<ScanScreen> {
 
     controller = CameraController(
       firstCamera,
-      ResolutionPreset.medium,
+      ResolutionPreset.high,
     );
 
     _initializeControllerFuture = controller.initialize();
@@ -36,17 +37,87 @@ class _ScanScreenState extends State<ScanScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(controller);
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CameraPreview(controller);
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(
+                        Icons.close_rounded,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.flash_on_rounded,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Image.asset("assets/images/scanner.png"),
+                const Spacer(),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 175,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset("assets/images/last-pic.png"),
+                      const CircleAvatar(
+                        backgroundColor: Color(0XFFFFC0B8),
+                        radius: 36,
+                        child: CircleAvatar(
+                          backgroundColor: Color(0XFFFF8473),
+                          radius: 20,
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: ColorConstant.secondary,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Icon(
+                          Icons.cached_rounded,
+                          color: Color(0XFFFF8473),
+                          size: 28,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
